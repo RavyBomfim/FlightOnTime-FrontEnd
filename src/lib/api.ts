34a9334d41e4e5cdab3512e4f128f911550c8fca
Marcelo <mcomanduci@gmail.com>
2026-01-17 19:24:30 -0300

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 console.log("API_BASE_URL em uso:", API_BASE_URL);
 
